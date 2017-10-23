0b6de71c4754b11167dd41ee93688120673ff802
rykun <rykunek@hotmail.com>
2017-10-23 23:58:12 +0200

    add scrollToBottom when new msg receives

--- src/components/messanger/message-list.js
+++ src/components/messanger/message-list.js
@@ -3,25 +3,52 @@ import _ from 'lodash';
 
 class MessageList extends Component {
 
+  scrollToBottom(element) {
+    if(element.childElementCount > 2){
+      const clientHeight = element.clientHeight;
+      const scrollTop = element.scrollTop;
+      const scrollHeight = element.scrollHeight;
+
+      const newMessage = element.lastChild;
+      const newMessageHeight = newMessage.clientHeight;
+      const lastMessageHeight = newMessage.previousSibling.clientHeight;
+      console.log(lastMessageHeight);
+      if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight  >= scrollHeight){
+        element.scrollTop = scrollHeight;
+      }
+    }
+    
+  }
+
+  componentDidMount() {
+    this.scrollToBottom(this.messageListElement);
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom(this.messageListElement);
+  }
+  
   renderMessages(messages) {
     return _.map(messages, message => {
       return (
         <li key={message.createdAt} className="message">
           <div className="message__title">
             <h4>{message.from}</h4>
-            <span>{message.createdAtFormmated}</span>
+            <span>{message.createdAtFormatted}</span>
           </div>
           <div className="message__body">
             <p>{message.text}</p>
           </div>
+
         </li>
       );
     });
-  }
-  
+  };
   render() {
     return (
-      <ol className="chat__messages">
+      <ol className="chat__messages" ref={(ol) => {
+        this.messageListElement = ol;
+      }}>
         {this.renderMessages(this.props.messages)}
       </ol>
     );
